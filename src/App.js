232c9676881287228308fa186d99e7fea259f7c7
Sreevatsa Anand <sreevatsa98@gmail.com>
2023-01-22 18:06:18 +0530

    Created Components and segregate the task into different components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCircleCheck, faPen, faTrashCan
-}from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {
+//   faCircleCheck, faPen, faTrashCan
+// }from '@fortawesome/free-solid-svg-icons'
 
 import './App.css';
+
+import AddTaskFrom from './components/AddTaskForm.jsx';
+import UpdateFrom from './components/UpdateForm.jsx';
+import ToDo from './components/ToDo.jsx';
+
 
 function App() {
 
@@ -76,105 +81,32 @@ function App() {
       <br /><br />
 
     {/* Update Task */}
-    
     {updateData && updateData ? (
-      <>
-      <div className="row">
-      <div className="col">
-        <input
-          value={ updateData && updateData.title }
-          onChange={ (e) => changeTask(e) }
-          className="form-control form-control-lg"
-        />
-      </div>
-      <div className="col-auto">
-        <button 
-        onClick={updateTask}
-        className="btn btn-lg btn-success mr-20">
-          Update
-        </button>
-        <button 
-        onClick={cancelUpdate}
-        className="btn btn-lg btn-danger">
-          Cancel
-        </button>
-      </div>
-      </div>
-      <br />
-      </>
+      <UpdateFrom 
+        updateData={updateData}
+        changeTask={changeTask}
+        updateTask={updateTask}
+        cancelUpdate={cancelUpdate}
+      />
     ):(
 
-      <>
-      {/* Add New Task */}
-      <div className="row">
-        <div className="col">
-          <input 
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-            className="form-control form-control-lg"
-          />
-        </div>
-        <div className="col-auto">
-          <button onClick={addTask} 
-          className="btn btn-lg btn-success">
-            Add Task
-          </button>
-        </div>
-      </div>
-      <br />
-      </>
+      <AddTaskFrom 
+        newTask={newTask}
+        setNewTask={setNewTask}
+        addTask={addTask}
+      />
+
     )}
 
       {/* Display TO-DO's */}
       {toDo && toDo.length ? '' : 'Wow! you have completed all the task'}
 
-      {toDo && toDo 
-        .sort((a, b) => a.id > b.id ? 1 : -1) //To sort Task in an Order
-        .map( (task, index ) =>{
-          return(
-            <React.Fragment key={task.id}>
-              
-              <div className="col taskBg">
-
-                <div className={task.status ? 'done' : ''}>
-                  <span className="taskNumber">{index + 1}</span>
-                  <span className="taskText">{task.title}</span>
-                </div>
-
-                <div className="iconsWrap">
-                  <span title="Completed / Not Completed"
-                    onClick={ (e) => markDone(task.id) }
-                  >
-                    <FontAwesomeIcon icon={faCircleCheck} />
-                  </span>
-
-                  {task.status ? null : ( 
-                    <span title="Edit" 
-                      onClick={ () => setUpdateData({
-                      id: task.id,
-                      title: task.title,
-                      status: task.status ? true : false
-                      })
-                    }
-                    >
-                      <FontAwesomeIcon icon={faPen} />
-                    </span>
-                  )}
-
-                  <span title="Delete"
-                    onClick={() => deleteTask(task.id)}
-                  >
-                    <FontAwesomeIcon icon={faTrashCan} />
-                  </span>
-                </div>
-
-              </div>
-
-
-            </React.Fragment>
-          )
-        })
-      }
+      <ToDo 
+        toDo={toDo}
+        markDone={markDone}
+        setUpdateData={setUpdateData}
+        deleteTask={deleteTask}
+      />
 
     </div>
   );
